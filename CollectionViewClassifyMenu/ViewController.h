cf9085e6c6294e5a588a4ee2521b46cf1f49e117
pifupro_iOS_admin <chenyl@pifupro.com>
2015-03-17 11:14:21 +0800

    Initial Commit

---
+++ CollectionViewClassifyMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewClassifyMenu
+//
+//  Created by chenyilong on 15/3/17.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionViewClassifyMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CollectionViewClassifyMenu
+//
+//  Created by chenyilong on 15/3/17.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
